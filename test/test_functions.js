dec8d91f3601085dfd91637fbc38bee29068e8fa
Foo Barstein <2209098+bloombar@users.noreply.github.com>
2020-04-21 13:21:32 -0400

    intentionally messed up tests

--- test/test_functions.js
+++ test/test_functions.js
@@ -5,12 +5,10 @@
 // STEP 0 - DO ANY IMPORTS
 
 // first import relevant modules
-import assert from 'assert'; // node's unit testing assertion library
+import assert from 'assert' // node's unit testing assertion library
 
 // import testable methods from custom class we're testing
-import {
-  baz1, baz2, buw1, buw2, buw3,
-} from '../src/functions_example/index';
+import { baz1, baz2, buw1, buw2, buw3 } from '../src/functions_example/index'
 
 // STEP 1 - DEFINE TEST GROUP
 // define a test group named 'Rectangle tests'
@@ -22,23 +20,22 @@ describe('Functions example tests', () => {
   before(() => {
     // put any code here that you want to run before ANY tests are run
     // console.log("About to run a batch of tests...");
-  });
+  })
 
   beforeEach(() => {
     // put any code here that you want to run before EACH tests is run
     // console.log("About to run a test...");
-  });
+  })
 
   afterEach(() => {
     // put any code here that you want to run after EACH tests is run
     // console.log("Just finished a test...");
-  });
+  })
 
   after(() => {
     // put any code here that you want to run after ALL tests have completed
     // console.log("Just finished a batch of tests...");
-  });
-
+  })
 
   // STEP 3 - DEFINE UNIT TESTS IN THIS GROUP
   // unit tests should be in isolation
@@ -46,30 +43,30 @@ describe('Functions example tests', () => {
   // write a message what the test should show
   it('2 + 2 should be 4', () => {
     // insert your test case code here
-    assert.equal('baz1! The sum of 2 and 2 is 4.', baz1(2, 2));
-  });
+    assert.equal('baz1! The sum of 2 and 2 is 4.', baz1(2, 2))
+  })
 
   // write a message what the test should show
   it('3 + 1 should be 4', () => {
     // insert your test case code here
-    assert.equal('baz2! The sum of 3 and 1 is 4.', baz2(3, 1));
-  });
+    assert.equal('baz2! The sum of 3 and 1 is 10.', baz2(3, 1))
+  })
 
   // write a message what the test should show
   it('The argument is 8', () => {
     // insert your test case code here
-    assert.equal('buw1! The argument is 8!', buw1(8));
-  });
+    assert.equal('buw1! The argument is 8!', buw1(8))
+  })
 
   // write a message what the test should show
   it('The argument is 5', () => {
     // insert your test case code here
-    assert.equal('buw2! The argument is 5!', buw2(5));
-  });
+    assert.equal('buw2! The argument is 5!', buw2(5))
+  })
 
   // write a message what the test should show
   it('The argument is 22', () => {
     // insert your test case code here
-    assert.equal('buw3! The argument is 22!', buw3(22));
-  });
-});
+    assert.equal('buw3! The argument is 22!', buw3(22))
+  })
+})
